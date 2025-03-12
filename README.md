a11938ee8f827408d275d5813c2570616b4f119f
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2025-03-11 23:06:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Atividades de Lógica de Programação e Algorítimos
+
+## Sobre o projeto
+
+Neste repositório estão armazenados as atividades da matéria de lógica de programação e algorítimos do primeiro siclo do curso tecnológico de análise e desenvolvimento de sistemas (ADS)
+
+## Sobre as linguagens
+
+Este repositório será criado utilizando as linguagens **rust**, **c**, **typescript** e **q#**
+
+### C lang
+
+Para criar os arquivos executáveis dos arquivos .c recomendo a utilização do **GCC**, caso você esteja utilizando Windows deve ser feita a utilização do **WinGW**, já para o debug utilizaremos o **GBD** que está incluso no WinGW, mas caso você esteja utilizando o GCC diretamente o download do GBD deve ser feito a parte
---
+++ c/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#define ex2
+
+#ifdef ex1
+int lerdados(int numero)
+{
+    do
+    {
+        printf("Digite um numero\n");
+        scanf(" %d", &numero);
+        if (numero <= 0)
+        {
+            printf("\7O numero digitado nao pode ser \"0\" nem um numero negativo\n");
+        };
+    } while (numero <= 0);
+    return numero // eu não havia colocado o "return" e não ocorreu nenhum erro, acho que isso tem a ver com "PONTEIROS"
+}
+
+int main(int argc, char const *argv[])
+{
+    int numero;
+    numero = lerdados(numero); // isso não faz muito sentido mas não sei como eu poderia fazer da forma certa no fluxograma
+    printf("O sucessor do numero digitado e\' %d e o antecessor e\' %d", numero + 1, numero - 1);
+    return 0;
+}
+#endif
+
+#ifdef ex2
+float lerdados(float numero)
+{
+    do
+    {
+        printf("Digite o valor do produto\n");
+        scanf("%f", &numero);
+        if (numero <= 0)
+        {
+            printf("\7O valor digitado nao pode ser \"0\" nem um numero negativo\n");
+        };
+    } while (numero <= 0);
+    return numero; // antes o código estava com problema pois eu havia esquecido de escrever o "return" ;-;
+}
+
+int main(int argc, char const *argv[])
+{
+    float preco;
+    preco = lerdados(preco); // isso não faz muito sentido mas não sei como eu poderia fazer da forma certa no fluxograma
+    printf("O valor do produto com o reajuste e\' %f reais", preco * 1.1);
+    return 0;
+}
+#endif
+
+#ifdef ex3
+int lerdados(int numero)
+{
+    do
+    {
+        printf("Digite um numero\n");
+        scanf(" %d", &numero);
+        if (numero <= 0)
+        {
+            printf("\7O numero digitado nao pode ser \"0\" nem um numero negativo\n");
+        };
+    } while (numero <= 0);
+}
+
+int main(int argc, char const *argv[])
+{
+    int numero;
+    numero = lerdados(numero); // isso não faz muito sentido mas não sei como eu poderia fazer da forma certa no fluxograma
+    printf("O sucessor do numero digitado e\' %d e o antecessor e\' %d", numero + 1, numero - 1);
+    return 0;
+}
+#endif
+
+#ifdef ex4
+int lerdados(int numero)
+{
+    do
+    {
+        printf("Digite um numero\n");
+        scanf(" %d", &numero);
+        if (numero <= 0)
+        {
+            printf("\7O numero digitado nao pode ser \"0\" nem um numero negativo\n");
+        };
+    } while (numero <= 0);
+}
+
+int main(int argc, char const *argv[])
+{
+    int numero;
+    numero = lerdados(numero); // isso não faz muito sentido mas não sei como eu poderia fazer da forma certa no fluxograma
+    printf("O sucessor do numero digitado e\' %d e o antecessor e\' %d", numero + 1, numero - 1);
+    return 0;
+}
+#endif
