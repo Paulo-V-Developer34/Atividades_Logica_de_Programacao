122db7d1109ded3c5856732a68565e921468808d
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2025-03-12 19:53:13 -0300

    cargo init

---
+++ rust/target/debug/deps/rust-01851dc11343ba6c.d
@@ -0,0 +1,5 @@
+C:\Users\paulo\OneDrive\Documentos\PAULO VICTOR\trabalhos\faculdade\siclo 1\logica_de_programacao\c_lang\ex1\rust\target\debug\deps\librust-01851dc11343ba6c.rmeta: src/main.rs
+
+C:\Users\paulo\OneDrive\Documentos\PAULO VICTOR\trabalhos\faculdade\siclo 1\logica_de_programacao\c_lang\ex1\rust\target\debug\deps\rust-01851dc11343ba6c.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/deps/rust-ac3bcaecdf48d116.d
@@ -0,0 +1,5 @@
+C:\Users\paulo\OneDrive\Documentos\PAULO VICTOR\trabalhos\faculdade\siclo 1\logica_de_programacao\c_lang\ex1\rust\target\debug\deps\librust-ac3bcaecdf48d116.rmeta: src/main.rs
+
+C:\Users\paulo\OneDrive\Documentos\PAULO VICTOR\trabalhos\faculdade\siclo 1\logica_de_programacao\c_lang\ex1\rust\target\debug\deps\rust-ac3bcaecdf48d116.d: src/main.rs
+
+src/main.rs:
