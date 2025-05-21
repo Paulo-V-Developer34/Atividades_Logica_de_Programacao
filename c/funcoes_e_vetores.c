75158bc89ef4db38dd2a3979f404a19e78ab812d
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2025-05-21 17:52:23 -0300

    finish funcoes_e_vetores

--- c/funcoes_e_vetores.c
+++ c/funcoes_e_vetores.c
@@ -1,5 +1,6 @@
+//Paulo Victor
 #include <stdio.h>
-#define ex3
+#define ex4
 
 #ifdef ex1
 int Positivo(float valor){
@@ -99,6 +100,59 @@ int main(int argc, char const *argv[])
     Soma(n1,n2, v_soma);
     Media(n1,n2, v_media);
     Produto(n1,n2, v_produto);
+
+    return 0;
+}
+#endif
+
+#ifdef ex4
+int Positivo(float valor){
+    if(valor > 0){
+        return 1; //positivo
+    } else {
+        return 0; //negativo
+    }
+}
+
+void Exiba(float valor, float p_imposto, float p_lucro){
+    float v_lucro = valor * (p_lucro/100);
+    float v_imposto = valor * (p_imposto/100);
+    float v_final = valor + v_imposto + v_lucro;
+    printf("\nO valor do imposto e\' R$%.4f, do lucro e\' R$%.4f e o custo final do produto e\' R$%.4f", v_imposto, v_lucro, v_final);
+}
+
+int main(int argc, char const *argv[])
+{
+    float v_produto, p_imposto, p_lucro, resultado; //v = valor, p = porcentagem
+    
+    do{
+        printf("\nDigite o valor do produto: ");
+        scanf("%f",&v_produto);
+        resultado = Positivo(v_produto);
+        if (resultado == 0){
+            printf("\nO valor digitado e invalido, digite novamente");
+        }
+    }while(resultado == 0);
+
+    do{
+        printf("\nDigite o valor do imposto em porcentagem: ");
+        scanf("%f",&p_imposto);
+        resultado = Positivo(p_imposto);
+        if (resultado == 0){
+            printf("\nO valor digitado e invalido, digite novamente");
+        }
+    }while(resultado == 0);
+
+    do{
+        printf("\nDigite o valor do lucro em porcentagem: ");
+        scanf("%f",&p_lucro);
+        resultado = Positivo(p_lucro);
+        if (resultado == 0){
+            printf("\nO valor digitado e invalido, digite novamente");
+        }
+    }while(resultado == 0);
+    
+    Exiba(v_produto, p_imposto, p_lucro);
 
     return 0;
 }
